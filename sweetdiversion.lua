48e147374588aff03e472b800d0e9544eb6878fc
Mark Riedesel <mark@klowner.com>
2014-04-03 13:14:14 -0500

    Removed GetLoveVersion() in favor of love._version_major, etc...

--- sweetdiversion.lua
+++ sweetdiversion.lua
@@ -3,32 +3,9 @@
 -- Copyright (c) 2014 Mark "Klowner" Riedesel
 --]]--------------------------------
 
-local sweetdiversion = {}
-local _love_version
 local _version_conditions = {}
 
-function sweetdiversion.GetLoveVersion()
-	local getVersion = love.getVersion
-	local version = _love_version
-
-	if version then
-		return unpack(version)
-	end
-
-	if getVersion then
-		version = {getVersion()}
-	else
-		version = {}
-		for v, _ in string.gmatch(love._version, "(%d+)") do
-			table.insert(version, tonumber(v))
-		end
-	end
-
-	_love_version = version -- cache result
-	return unpack(version)
-end
-
-function sweetdiversion.LoveVersionIs(expr)
+local sweetdiversion = function (expr)
 	local cache = _version_conditions
 	local match = cache[expr]
 
@@ -36,7 +13,7 @@ function sweetdiversion.LoveVersionIs(expr)
 		return match
 	end
 
-	local curr_version_t = {sweetdiversion.GetLoveVersion()}
+	local curr_version_t = {love._version_major, love._version_minor, love._version_revision}
 	local op = string.sub(expr, 0, 1)
 	local comparitor
 
@@ -65,10 +42,5 @@ function sweetdiversion.LoveVersionIs(expr)
 	cache[expr] = result
 	return result
 end
-
--- syntactic sugar, so you can use `if sweetdiversion('0.9.*') then...'
-setmetatable(sweetdiversion, {
-	__call = function (cls, expr) return cls.LoveVersionIs(expr) end
-})
 
 return sweetdiversion
